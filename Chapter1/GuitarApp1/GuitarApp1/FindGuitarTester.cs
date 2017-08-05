34cb118b2d3897e7d3ec26edb0abc8048505080b
patnir <rahul20patni@gmail.com>
2017-08-05 14:20:43 -0400

    adding gitignore

---
+++ Chapter1/GuitarApp1/GuitarApp1/FindGuitarTester.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarApp1
+{
+	class FindGuitarTester
+	{
+		public static void Test1()
+		{
+			Inventory inventory = new Inventory();
+			initializeInventory(inventory);
+
+			GuitarSpec whatErinLikes = new GuitarSpec(Builder.FENDER, "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER, 12);
+			List<Guitar> matches= inventory.search(whatErinLikes);
+
+			if (matches.Count != 0)
+			{
+				foreach(Guitar guitar in matches)
+					guitar.print();
+			}
+			else
+			{
+				Console.WriteLine("We have nothing for you");
+			}
+		}
+
+		public static void initializeInventory(Inventory inventory)
+		{
+			inventory.addGuitar("V95693", 1499.95, Builder.FENDER, "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER, 6);
+			inventory.addGuitar("V95693", 1699.95, Builder.FENDER, "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER, 12);
+
+			inventory.addGuitar("V95693", 1599.95, Builder.FENDER, "Stratocastor", Type.ACOUSITC, Wood.ALDER, Wood.CEDAR, 12);
+			inventory.addGuitar("V95693", 1799.95, Builder.FENDER, "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER, 6);
+
+		}
+	}
+}
---
+++ Chapter1/GuitarApp1/GuitarApp1/GuitarSpec.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarApp1
+{
+	public class GuitarSpec
+	{
+		private Builder builder;
+		private Type type;
+		private Wood backWood;
+		private Wood topWood;
+		private string model;
+		private int numStrings;
+
+
+		public GuitarSpec(Builder builder, string model, Type type, Wood backWood,
+			Wood topWood, int numStrings)
+		{
+			this.builder = builder;
+			this.type = type;
+			this.topWood = topWood;
+			this.backWood = backWood;
+			this.model = model;
+			this.numStrings = numStrings;
+		}
+
+		public bool Equals(GuitarSpec spec)
+		{
+			if (this.type != spec.type)
+				return false;
+			if (this.builder != spec.builder)
+				return false;
+			if (this.backWood != spec.backWood)
+				return false;
+			if (this.topWood != spec.topWood)
+				return false;
+			if (this.numStrings != spec.numStrings)
+				return false;
+			if (spec.model == null || spec.model == "" || spec.model != this.model)
+				return false;
+			return true;
+		}
+
+
+		public void print()
+		{
+			Console.WriteLine(
+				this.builder + " " + this.model + " " + this.type + " guitar:\n"
+			+ this.numStrings.ToString() + " strings with " + this.backWood + " back and sides, and " + this.topWood + " top.");
+		}
+
+		public string getModel()
+		{
+			return this.model;
+		}
+
+		public int getNumStrings()
+		{
+			return this.numStrings;
+		}
+
+		public Builder getBuilder()
+		{
+			return this.builder;
+		}
+		public Wood getTopWood()
+		{
+			return this.topWood;
+		}
+		public Type getType()
+		{
+			return this.type;
+		}
+
+		public Wood getBackWood()
+		{
+			return this.backWood;
+		}
+	}
+}
