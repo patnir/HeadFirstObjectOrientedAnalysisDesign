996e81aae9b74390850df2e116ee84f6e756430b
patnir <rahul20patni@gmail.com>
2017-08-04 20:29:57 -0400

    v1

---
+++ Chapter1/GuitarApp1/GuitarApp1/Guitar.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarApp1
+{
+	public class Guitar
+	{
+		public string serialNumber;
+		private string builder;
+		private string model;
+		private string type;
+		private string backWood;
+		private string topWood;
+		private double price;
+
+		public Guitar(string serialNumber, double price,
+			string builder, string model, string type, string backWood,
+			string topWood)
+		{
+			this.serialNumber = serialNumber;
+			this.price = price;
+			this.builder = builder;
+			this.model = model;
+			this.type = type;
+			this.topWood = topWood;
+			this.backWood = backWood;
+		}
+
+		public string getSerialNumber()
+		{
+			return this.serialNumber;
+		}
+
+		public string getBuilder()
+		{
+			return this.builder;
+		}
+
+		public void setPrice(float newPrice)
+		{
+			this.price = newPrice;
+		}
+
+		public string getModel()
+		{
+			return this.model;
+		}
+
+		public string getTopWood()
+		{
+			return this.topWood;
+		}
+
+		public string getType()
+		{
+			return this.type;
+		}
+
+		public string getBackWood()
+		{
+			return this.backWood;
+		}
+	}
+}
---
+++ Chapter1/GuitarApp1/GuitarApp1/Inventory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarApp1
+{
+	public class Inventory
+	{
+		private LinkedList<Guitar> guitars;
+
+		public void addGuitar(string serialNumber, double price,
+			string builder, string model, string type, string backWood,
+			string topWood)
+		{
+			Guitar guitar = new Guitar(serialNumber, price, builder, model, type, backWood, topWood);
+			guitars.AddLast(guitar);
+		}
+
+		public Guitar getGuitar(string serialNumber)
+		{
+			foreach (Guitar g in this.guitars)
+			{
+				if (g.getSerialNumber() == serialNumber)
+				{
+					return g;
+				}
+			}
+
+			return null;
+		}
+
+		public Guitar search(Guitar searchGuitar)
+		{
+
+			return null;
+		}
+
+		public bool compareStrings(string one, string two)
+		{
+			if (one != null && one != "" && one == two)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
