8de812d1ba52a705aa6bd24894227806da2fc3ec
patnir <rahul20patni@gmail.com>
2017-08-05 14:26:40 -0400

    chapter 2 customer erquirements set up

---
+++ Chapter2/DougDogDoor/DougDogDoor/DogDoor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DougDogDoor
+{
+	class DogDoor
+	{
+		private bool open;
+
+		public DogDoor()
+		{
+			open = false;
+		}
+
+		public void openDogDoor()
+		{
+			Console.WriteLine("The dog door opens");
+			open = true;
+		}
+
+		public void closeDogDoor()
+		{
+			Console.WriteLine("The dog door closes");
+			open= false;
+		}
+
+		public bool isOpen()
+		{
+			return open;
+		}
+	}
+}
